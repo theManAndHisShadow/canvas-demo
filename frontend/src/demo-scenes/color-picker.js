b6ae0dc06943de33f0f156d0776317669490c979
theManAndHisShadow <justemailpostbox@gmail.com>
2024-08-02 01:53:48 +0500

    [Scene]: added color picker feature to 'color-picker' scene.

--- frontend/src/demo-scenes/color-picker.js
+++ frontend/src/demo-scenes/color-picker.js
@@ -5,7 +5,12 @@ let colorPicker = new Scene({
         'description': {
             type: 'display-infobox',
             label: 'Description',
-            text: 'empty'
+            text: 'Color picker based on multicolor radial gradient and mouse events. Move range slider to adjust color. Click on the desired location on the circle to get color data.'
+        },
+
+        'currentColor': {
+            type: 'display',
+            label: 'Current color',
         },
 
         'adjustment-slider': {
@@ -93,14 +98,72 @@ let colorPicker = new Scene({
         settings.setState('adjustment-slider', defaultValue);
         draw(defaultValue);
 
+        // subscribing to settings changes
         settings.subscribe((key, newValue, oldValue) => {
             let value = newValue / 100;
 
             // redraw spectrum
             draw(value);
         });
+
+
+        // color picker feature values
+        let mouseOnCircle = false;
+        let mousePos = false;
+
+        // updating values on mouse move
+        canvas.addEventListener('mousemove', (event) => {
+            mousePos = getMousePos(canvas, event);
+
+            // if distance - radius less than 0 - mouse cursor is under a spectrum circle
+            let d = getDistanseBetweenTwoPoint(mousePos.x, mousePos.y, canvas.width / 2, canvas.height / 2);
+            mouseOnCircle = d - radius < 0 ? true : false;            
+
+            // update mouse cursor when its on spectrum circle
+            canvas.style.cursor = mouseOnCircle ? 'crosshair' : 'inherit';
+        });
+
+        // updating color on click at canvas
+        canvas.addEventListener('click', (event) => {
+            // get pixel data
+            let rawData = context.getImageData(mousePos.x, mousePos.y, 1, 1).data;
+
+            // re-collect to rgb color string
+            let color = `rgb(${rawData[0]}, ${rawData[1]}, ${rawData[2]})`;
+            
+            // update value of 'Current color' display option
+            display.updateValue
+            ('currentColor', 
+                // check is mouse under a spectrum circle
+                mouseOnCircle ? 
+
+                // some DARK CSS MAGIC xD
+
+                // if true - draw round colored element with color string text
+                `<span style="
+                    background: ${color}; 
+                    width: 7px; 
+                    height: 7px; 
+                    position: relative;
+                    display: inline-block;
+                    border-radius: 100%; 
+                    border: 2px solid rgba(0, 0, 0, 0.25); 
+                    padding: 1px; 
+                    left: -2px; 
+                    top: 2px;
+                "></span>${color}` 
+
+                // else - draw pale colored mockup
+                : '<i class="pale">no info</i>'
+            );
+        });
     }
 });
+
+
+/**
+ * TODO: check bug with scene switching 'static-gradients' <---> 'color-picker'
+ */
 
 // Exproting scene
 window.exportedObjects.push(colorPicker);
