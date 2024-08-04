a47a77b31bb753f862518f18ea56e1586829a687
theManAndHisShadow <justemailpostbox@gmail.com>
2024-08-04 18:49:05 +0500

    [Scene]: The old code has been refactored, the field is now infinite (as far as possible).
    Changelog:
    - '.drawAxisPointMarks()' method removed now;
    - '.drawGrid()' method is updated - now it can render two grid - minor grid and major grid;
    - also new realisation allows to render field endlessly (presumably);
    - added new instance property - 'globalOfsset';
    - '.move()' method updated;
    - updated grid color.

--- frontend/src/demo-scenes/simple-funсion-graphs.js
+++ frontend/src/demo-scenes/simple-funсion-graphs.js
@@ -25,8 +25,10 @@ let simpleFunctionGraphs = new Scene({
             renderer: context,
             cx: width / 2,
             cy: height / 2,
+
+            // check grid cell size bug when value is not 10
             gridCellSize: 10,
-            gridLineColor: 'rgba(55, 55, 55, 0.35)',
+            gridLineColor: 'rgba(35, 35, 35, 0.35)',
             gridLineThickness: 1,
             axisColor: 'white',
             fillColor: 'black',
@@ -95,6 +97,11 @@ class CartesianField {
         this.cx = cx;
         this.cy = cy;
 
+        this.globalOffset = {
+            x: 0,
+            y: 0,
+        };
+
         this.renderer = renderer;
         this.viewWidth = renderer.canvas.width;
         this.viewHeight = renderer.canvas.height;
@@ -123,8 +130,14 @@ class CartesianField {
      * @param {number} sensitivity - sensitivity of mouse movements
      */
     move(offset, sensitivity = 1){
-        this.cx = this.cx + (offset.x * sensitivity);
-        this.cy = this.cy + (offset.y * sensitivity);
+        let xOffset = Math.round(offset.x * sensitivity);
+        let yOffset = Math.round(offset.y * sensitivity);
+
+        this.cx = this.cx + xOffset;
+        this.cy = this.cy + yOffset;
+
+        this.globalOffset.x = this.globalOffset.x + xOffset;
+        this.globalOffset.y = this.globalOffset.y + yOffset;
     }
 
 
@@ -134,69 +147,18 @@ class CartesianField {
      * @param {Number} param.y - text y pos
      * @param {string} param.text - text
      * @param {string} param.fontFamily - text font family
-     * @param {string} param.fontSize - text font size
+     * @param {number} param.fontSize - text font size
      * @param {string} param.fontWeight - text font weight
      * @param {string} param.align - text align (left, center, right)
      * @param {string} param.baseline - text baseline
      */
-    drawText({x, y, text, color ='white', fontFamily = 'Arial', fontSize = '12px', fontWeight = '', align = 'center', baseline = 'middle'}){
+    drawText({x, y, text, color ='white', fontFamily = 'Arial', fontSize = 12, fontWeight = '', align = 'center', baseline = 'middle'}){
         this.renderer.fillStyle = color;
-        this.renderer.font = `${fontWeight} ${fontSize} ${fontFamily}`; 
+        this.renderer.font = `${fontWeight} ${fontSize}px ${fontFamily}`; 
         this.renderer.textAlign = align;
         this.renderer.textBaseline = baseline;
 
         this.renderer.fillText(text, x, y);
-    }
-
-
-    /**
-     * Draws scale elements on axes (numbers and little lines)
-     */
-    drawAxisPointMarks(){
-        this.renderer.strokeStyle = 'red' || this.axisColor;
-        this.renderer.lineWidth = 1;
-
-        let markLineWidth = 4;
-
-        // y-axis markers
-        this.renderer.beginPath();
-        for(let i = this.viewHeight - (this.gridCellSize*2), n = -1; i > 0; i -= this.gridCellSize * 4, n++) {
-            let correctionY = this.cy - (this.viewHeight / 2);
-            let y = correctionY + i - (this.subpixel + this.gridCellSize * 2) + 1;
-
-            this.drawText({
-                x: this.cx - (markLineWidth / 2) - 5,
-                y: n - 3 == 0 ? y + 11 : y,
-                text: n - 3,
-                color: 'red',
-            });
-
-            this.renderer.moveTo(this.cx - (markLineWidth / 2), y);
-            this.renderer.lineTo(this.cx + (markLineWidth / 2) + 2, y);
-        }
-        this.renderer.stroke();
-
-        // x-axis markers
-        this.renderer.strokeStyle = 'cyan' || this.axisColor;
-        this.renderer.beginPath();
-        for(let j = this.viewWidth - (this.gridCellSize * 2), n = -1; j > 0; j -= this.gridCellSize * 4, n++) {
-            let correctionX = (this.cx - (this.viewWidth / 2));
-            let x = correctionX + j + this.subpixel;
-
-            this.drawText({
-                x: x,
-                y: this.cy + (markLineWidth / 2) + 10,
-                text: n - 6 == 0 ? '' : (n - 6) * -1,
-                color: 'cyan',
-            });
-            
-            this.renderer.moveTo(x, this.cy - (markLineWidth / 2));
-            this.renderer.lineTo(x, this.cy + (markLineWidth / 2));
-        }
-        this.renderer.stroke();
-
-        this.renderer.closePath();
-
     }
 
 
@@ -224,37 +186,141 @@ class CartesianField {
     
 
     /**
-     * Draws a field grid
+     * Draws a field grid.
      */
-    drawGrid() {
+    drawGrid(){
+        // function basic values
         const width = this.viewWidth;
         const height = this.viewHeight;
-        this.renderer.strokeStyle = this.gridLineColor;
+        
+        
+        // color selected by user from class costructor
+        const minorGridLineColor = this.gridLineColor;
+
+        // rgba -> array
+        const parsed = minorGridLineColor.split(', ')
+                                         .map(item => {
+                                             return Number(
+                                                 item.replace(/(\(|\)|rgba|rgb)/gm, '')
+                                             );
+                                          });
+        
+        // increasing the color value of the major line                           
+        const gain = 0.45;                              
+        const modifined = parsed.map((item, i) => {
+            return i == 3 && item <= 1 ? parsed[i] + gain : item;
+        });
+
+        // ready RGBA string
+        const majorGridLineColor = 'rgba(' + modifined + ')';
+        
+        // thickness of grid line 
         this.renderer.lineWidth = this.gridLineThickness;
 
+       // local helper function
+       const drawPart = (start, type) => { 
+            /**
+             * N.B.: 
+             * I did not use a custom helper function to avoid multiple calls to the context 
+             * and not to worsen the already not excellent performance
+            */
 
+            // draw a major line
+            this.renderer.beginPath();
+            this.renderer.strokeStyle = majorGridLineColor;
+        
+            // vertical major line
+            if (type === 'column') {
+                let x = start + (this.gridCellSize * 2) + this.subpixel;
+                this.renderer.moveTo(x, 0);
+                this.renderer.lineTo(x, height);
+
+            // horizontal line
+            } else if (type === 'row') {
+                let y = start + this.subpixel;
+                this.renderer.moveTo(0, y);
+                this.renderer.lineTo(width, y);
+            }
+        
+            this.renderer.stroke();
+            this.renderer.closePath();
+        
+            // draw a minor three lines 
+            this.renderer.beginPath();
+            this.renderer.strokeStyle = minorGridLineColor;
+        
+            for (let i = 3; i <= 5; i++) {
+                if (type === 'column') {
+                    let x = start + (this.gridCellSize * i) + this.subpixel;
+                    this.renderer.moveTo(x, 0);
+                    this.renderer.lineTo(x, height);
+                } else if (type === 'row') {
+                    let y = start + (this.gridCellSize * (i - 2)) + this.subpixel;
+                    this.renderer.moveTo(0, y);
+                    this.renderer.lineTo(width, y);
+                }
+            }
+        
+            this.renderer.stroke();
+            this.renderer.closePath();
+        };
+        
+        // axis scale number label font size
+        const fontSize = 11;
+
+        // minor step = this.gridCellSize
+        // but each part contain 1 major laine + space + three minor lines
+        const majorStep = this.gridCellSize * 4;
+        
         /**
-         * I did not use a custom helper function to avoid multiple calls to the context 
-         * and not to worsen the already not excellent performance
+         * Every time the screen shifts by more than the width of a column or the height of a row (major step), 
+         * the renderer takes into account the displacement and draws an additional section in advance, 
+         * which is why we need to know what indentation is in the columns/rows now.
          */
-        this.renderer.beginPath();
-    
-        // Only draw lines that are within the visible area
-        const startX = Math.floor((this.cx % this.gridCellSize) - this.gridCellSize);
-        const startY = Math.floor((this.cy % this.gridCellSize) - this.gridCellSize);
-    
-        for (let x = startX; x < width; x += this.gridCellSize) {
-            this.renderer.moveTo(x + this.subpixel, 0);
-            this.renderer.lineTo(x + this.subpixel, height);
+
+        let originX = this.cx - (width / 2);
+        let columnsOffset = Math.trunc(this.globalOffset.x / majorStep);
+        for (let i = -1 - columnsOffset; i < (width / majorStep) - columnsOffset; i++) {
+            // column start x pos
+            let x = originX + (i * majorStep);
+            let number = (i - 7) * 2;
+
+            // draw a group of vertical lines - column
+            // each column contains one major line ant three minor
+            drawPart(x, 'column');
+
+            // draw a text with number of axis scale
+            this.drawText({
+                x: x + (majorStep / 2),
+                y: this.cy -10,
+                // hide '0' pos number label to x-axis
+                // beacuse we already using '0' pos label from y-axis
+                text: number == 0 ? '' : number,
+                fontSize: fontSize,
+            });
         }
-    
-        for (let y = startY; y < height; y += this.gridCellSize) {
-            this.renderer.moveTo(0, y + this.subpixel);
-            this.renderer.lineTo(width, y + this.subpixel);
+        
+        let originY = this.cy - (height / 2);
+        let rowsOffset = Math.trunc(this.globalOffset.y / majorStep);
+        for (let j = -1 - rowsOffset; j < (height / majorStep) - rowsOffset + 1; j++) {
+            // row start y pos
+            let y = originY + (j * majorStep);
+            let number = (j - 5) * 2 * -1;
+
+            // draw a group of horizontal lines - row
+            // each row contains one major line ant three minor
+            drawPart(y, 'row');
+
+            // draw a text with number of axis scale
+            this.drawText({
+                x: this.cx - 7,
+                // different spacing to '0' pos number label and other numbers
+                y: number == 0 ? y + 12 : y + 1,
+                text: number,
+                fontSize: fontSize,
+                align: 'right',
+            });
         }
-    
-        this.renderer.closePath();
-        this.renderer.stroke();
     }
 
 
@@ -280,6 +346,5 @@ class CartesianField {
         this.fill();
         this.drawGrid();
         this.drawAxis();
-        this.drawAxisPointMarks();
     }
 }
