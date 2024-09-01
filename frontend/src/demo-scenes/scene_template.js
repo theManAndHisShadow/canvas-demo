81350f6fe1a2f263a9cd5c8f24fad38eac7500e0
theManAndHisShadow <justemailpostbox@gmail.com>
2024-09-01 10:47:46 +0500

    [Scene]: added empty template scene.

---
+++ frontend/src/demo-scenes/scene_template.js
@@ -0,0 +1,38 @@
+let colorPicker = new Scene({
+    title: 'Scene template', 
+
+    ui: {
+        'description': {
+            type: 'display-infobox',
+            label: 'Description',
+            text: 'Empty',
+        },
+    },
+
+    code: (root, display, settings) => {
+        // clearing prev created animation threads
+        window.runningAnimations.clearQueue();
+
+        // timestamp to current scene's canvas elem
+        const timestamp = Date.now();
+
+        // reset the element state to remove all previously applied event handlers
+        let canvas = resetElement(root.querySelector('canvas'), `canvas-${timestamp}`);
+        
+        // basic canvas values
+        const width = 600;
+        const height = 400;
+        canvas.width = width;
+        canvas.height = height;
+
+        const context = canvas.getContext('2d');
+    }
+});
+
+
+// Exproting scene
+window.exportedObjects.push(colorPicker);
+
+/**
+* Scene file internal helper function defenitions
+*/
