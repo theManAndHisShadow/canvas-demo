6b9b3697d5635877a7f6e8bb8ac01de0f943a28c
theManAndHisShadow <justemailpostbox@gmail.com>
2024-10-03 16:18:19 +0500

    [UI] updated 'UI_HUD' class.
    Changelog:
    - now 'UI_HUD' class extends 'UI_BlockPrototype' class;
    - added own '.render()' method.

--- src/js/core/classes/UI/ui_hud.class.js
+++ src/js/core/classes/UI/ui_hud.class.js
@@ -1,5 +1,11 @@
-export default class UI_HUD {
-    constructor(){
+import UI_BlockPrototype from "./ui_block.prototype_class";
+
+export default class UI_HUD extends UI_BlockPrototype{
+    constructor(htmlRootElementRef){
+        super(htmlRootElementRef);
+
         // ...
     }
+
+    render() {}
 }
