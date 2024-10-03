d3624d8d5fcdcc8ec796a60675243e2416b241a1
theManAndHisShadow <justemailpostbox@gmail.com>
2024-10-03 16:09:41 +0500

    [UI]: added UI block prototype class.

---
+++ src/js/core/classes/UI/ui_block.prototype_class.js
@@ -0,0 +1,47 @@
+import SyntheticEventTarget from "../synthetic_event_target.class";
+
+/**
+ * Base class for various UI sections.
+ */
+export default class UI_BlockPrototype extends SyntheticEventTarget {
+    constructor(htmlRootElementRef) {
+        super();
+
+        this.html = {
+            root: htmlRootElementRef || null,
+        };
+    }
+
+
+    /**
+     * Appends new ready HTML element to HTML root block
+     * @param {string} elementName - target element name 
+     * @param {{element: HTMLElement, label: HTMLElement, value: HTMLElement}} renderedElementObject - {element - ref to whole rendered element, label - ref to label, value - ref to value element}
+     */
+    appendToHTML(elementName, renderedElementObject) {
+        let { element } = renderedElementObject;
+
+        this.html[elementName] = renderedElementObject;
+        this.html.root.appendChild(element);
+    }
+
+
+    /**
+    * Updates value of target element of instance
+    * @param {string} elementName - name of element (key of this.html object)
+    * @param {number|string|boolean} newValue
+    */
+    updateValue(elementName, newValue) {
+        this.html[elementName].innerHTML = newValue;
+    }
+
+
+    /**
+    * Resets content of root element in HTML
+    */
+    clearRoot() {
+        this.html.root.innerHTML = '';
+    } 
+
+
+}
